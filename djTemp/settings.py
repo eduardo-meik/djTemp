aa2b653566998a7ce0e38add908198ed1d597032
eduardo-meik <eduardo@meiklabs.com>
2023-09-07 23:04:34 -0300

    database config

--- djTemp/settings.py
+++ djTemp/settings.py
@@ -77,7 +77,11 @@ WSGI_APPLICATION = "djTemp.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "NAME": "meiklabs_djTemp",
+        "HOST": "localhost",
+        "PORT": "3306",
+        "USER": "meiklabs_superadmin",
+        "PASSWORD": "h0}94gtm~j-E",
     }
 }
 
